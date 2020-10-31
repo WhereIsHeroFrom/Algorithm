43434f24f4f2c944558a8117d235a23ee4b050e4
WhereIsHeroFrom <menjitianya2007@163.com>
2020-10-31 23:07:36 +0800

    矩阵快速幂的题

--- 二分/矩阵二分快速幂/HDU 5895 Mathematician QSC.cpp
+++ 二分/矩阵二分快速幂/HDU 5895 Mathematician QSC.cpp
@@ -18,7 +18,7 @@ Algorithm Complexity: O(sqrt(n))
 #include <vector>
 using namespace std;
 
-#define MAXP 65540
+#define MAXP 10010
 #define LL __int64
 
 int primes[MAXP];
@@ -90,85 +90,141 @@ LL ExtendPhi(LL a, LL b, LL c) {
 	return Exp(a, b % ph + ph, c);
 }
 
-#define MAXN 6
-
+#define MAXN 4
 #define LL __int64
-
 LL MOD = 1234567891;
 
 class Matrix {
-public:
+private:
 	int n, m;
-	LL d[MAXN][MAXN];
-	Matrix() {
-		n = m = 0;
-		int i, j;
-		for (i = 0; i < MAXN; i++) {
-			for (j = 0; j < MAXN; j++) {
-				d[i][j] = 0;
-			}
+	LL** pkData;
+
+	static Matrix s_kMatrix[64], s_kMultiplyTemp;
+
+	void _Alloc() {
+		// 之所以把空间放在堆上，是因为当MAXN很大时，栈上分配不了这么多空间
+		pkData = new LL *[MAXN];
+		for (int i = 0; i < MAXN; ++i) {
+			pkData[i] = new LL[MAXN];
 		}
-	}
-	Matrix operator *(const Matrix& other) {
-		Matrix ret;
-		ret.n = n;
-		ret.m = other.m;
-		int i, j, k;
-		for (j = 0; j < ret.m; j++) {
-			for (i = 0; i < ret.n; i++) {
-				ret.d[i][j] = 0;
-				for (k = 0; k < m; k++) {
-					ret.d[i][j] += d[i][k] * other.d[k][j];
-					if (ret.d[i][j] >= MOD)
-						ret.d[i][j] %= MOD;
-				}
-			}
-		}
-		return ret;
 	}
 
-	Matrix Identity(int _n) {
-		Matrix I;
-		I.n = I.m = _n;
+	void _Dealloc() {
+		if (pkData) {
+			// 先释放低维空间，再释放高维的
+			for (int i = 0; i < MAXN; ++i) {
+				delete[] pkData[i];
+			}
+			delete[] pkData;
+			pkData = NULL;
+		}
+	}
+public:
+	Matrix() : n(0), m(0), pkData(NULL) {
+		_Alloc();
+	}
+	virtual ~Matrix() {
+		_Dealloc();
+	}
+
+	LL get(int i, int j) const {
+		return pkData[i][j];
+	}
+
+	int getN() const {
+		return n;
+	}
+
+	static void Identity(int _n, Matrix& ret) {
+		ret.n = ret.m = _n;
 		int i, j;
 		for (i = 0; i < _n; i++) {
 			for (j = 0; j < _n; j++) {
-				I.d[i][j] = (i == j) ? 1 : 0;
+				ret.pkData[i][j] = (i == j) ? 1 : 0;
 			}
 		}
-		return I;
 	}
 
-	Matrix getPow(unsigned __int64 e) {
-		Matrix tmp = *this;
-		Matrix ret = Identity(n);
+	static void GetPow(Matrix& raw, unsigned __int64 e, Matrix& ret) {
+		int index = 1;
+		Matrix::Identity(raw.getN(), ret);       // 0次幂
+		Matrix::s_kMatrix[1].Copy(raw);          // 1次幂
 		while (e) {
 			if (e & 1) {
-				ret = ret * tmp;
+				Matrix::Multiply(ret, Matrix::s_kMatrix[index], Matrix::s_kMultiplyTemp);
+				ret.Copy(Matrix::s_kMultiplyTemp);
 			}
 			e >>= 1;
-			tmp = tmp * tmp;
+			Matrix::Multiply(Matrix::s_kMatrix[index], Matrix::s_kMatrix[index], Matrix::s_kMatrix[index + 1]);
+			++index;
 		}
-		return ret;
 	}
+
+	static void Multiply(const Matrix &me, const Matrix& other, Matrix& ret) {
+		// assert(m == other.n);
+		ret.Reset(me.n, other.m);
+		int i, j, k;
+		for (i = 0; i < me.n; i++) {
+			for (k = 0; k < me.m; k++) {
+				LL v = me.pkData[i][k];
+				for (j = 0; j < other.m; j++) {
+					ret.pkData[i][j] += v * other.pkData[k][j];
+					if (ret.pkData[i][j] >= MOD) {
+						ret.pkData[i][j] %= MOD;
+					}
+				}
+			}
+		}
+	}
+
+	void Reset(int nn, int mm, LL data[][MAXN]) {
+		n = nn;
+		m = mm;
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j) {
+				pkData[i][j] = data[i][j];
+			}
+		}
+	}
+
+	void Reset(int nn, int mm) {
+		n = nn;
+		m = mm;
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j) {
+				pkData[i][j] = 0;
+			}
+		}
+	}
+
+	void Copy(const Matrix& other) {
+		n = other.n;
+		m = other.m;
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j) {
+				pkData[i][j] = other.pkData[i][j];
+			}
+		}
+	}
+
+
 
 	// | A  A |
 	// | O  I |
+	// I 需要手动调用 Identity(n);
 	// 扩展矩阵用于求A + A^2 + A^3 + ... + A^n
-	Matrix getExtendMatrix() {
-		Matrix ret;
+	void getExtendMatrix(Matrix& ret, Matrix& I) {
 		ret.n = ret.m = n * 2;
 		ret.copyMatrix(*this, 0, 0);
 		ret.copyMatrix(*this, 0, n);
-		ret.copyMatrix(Identity(n), n, n);
-		return ret;
+		ret.copyMatrix(I, n, n);
 	}
 
 	// 将矩阵A拷贝到当期矩阵的(r, c)位置
-	void copyMatrix(Matrix A, int r, int c) {
+	void copyMatrix(Matrix& A, int r, int c) {
 		for (int i = r; i < r + A.n; i++) {
 			for (int j = c; j < c + A.n; j++) {
-				d[i][j] = A.d[i - r][j - c];
+				pkData[i][j] = A.pkData[i - r][j - c];
 			}
 		}
 	}
@@ -177,12 +233,31 @@ public:
 		int i, j;
 		for (i = 0; i < n; i++) {
 			for (j = 0; j < m; j++) {
-				printf("%d ", d[i][j]);
+				printf("%d ", pkData[i][j]);
 			}
 			puts("");
 		}
 	}
 };
+
+Matrix Matrix::s_kMatrix[64];
+Matrix Matrix::s_kMultiplyTemp;
+
+LL AArray[][MAXN] = {
+	{ 1, 4, 4, 1 },
+	{ 0, 4, 4, 1 },
+	{ 0, 2, 1, 0 },
+	{ 0, 1, 0, 0 },
+};
+
+LL BArray[][MAXN] = {
+	{ 1 },
+	{ 1 },
+	{ 0 },
+	{ 0 },
+};
+
+
 
 
 #define MAXG 25
@@ -190,6 +265,7 @@ LL f[MAXG];
 LL g[MAXG];
 
 LL n, y, x, s;
+
 
 int main() {
 	f[0] = 0;
@@ -204,20 +280,9 @@ int main() {
 		g[i] = g[i - 1] + f[i] * f[i];
 		//printf("%d %I64d\n", i, g[i]);
 	}
-	Matrix A;
-	A.n = A.m = 4;
-	A.d[0][0] = 1; A.d[0][1] = 4; A.d[0][2] = 4; A.d[0][3] = 1;
-	A.d[1][0] = 0; A.d[1][1] = 4; A.d[1][2] = 4; A.d[1][3] = 1;
-	A.d[2][0] = 0; A.d[2][1] = 2; A.d[2][2] = 1; A.d[2][3] = 0;
-	A.d[3][0] = 0; A.d[3][1] = 1; A.d[3][2] = 0; A.d[3][3] = 0;
-
-	Matrix B;
-	B.n = 4; B.m = 1;
-	B.d[0][0] = 1;
-	B.d[1][0] = 1;
-	B.d[2][0] = 0;
-	B.d[3][0] = 0;
-
+	Matrix A, B, ret, tmp;
+	A.Reset(4, 4, AArray);
+	B.Reset(4, 1, BArray);
 	int t;
 	scanf("%d", &t);
 
@@ -230,7 +295,9 @@ int main() {
 		else {
 			LL sph = Phi(s + 1);
 			MOD = sph;
-			LL v = Exp(x, (A.getPow(n - 1) * B).d[0][0] + sph, s + 1);
+			Matrix::GetPow(A, n - 1, ret);
+			Matrix::Multiply(ret, B, tmp);
+			LL v = Exp(x, tmp.get(0, 0) + sph, s + 1);
 			printf("%I64d\n", v);
 		}
 
