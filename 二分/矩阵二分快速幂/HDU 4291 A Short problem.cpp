c86ab80779ca9d72dddd737e24015f56ae944e47
WhereIsHeroFrom <menjitianya2007@163.com>
2020-11-10 07:57:08 +0800

    循环节预处理 + 矩阵二分

---
+++ 二分/矩阵二分快速幂/HDU 4291 A Short problem.cpp
@@ -0,0 +1,290 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+
+/*
+矩阵二分快速幂
+递推公式的神级加速，转换成矩阵幂求解；
+矩阵相乘的最内层循环加和如果不溢出，则憋着不取模；
+Author: WhereIsHeroFrom
+Update Time: 2020-11-03
+Algorithm Complexity: O(m^3log(n))
+*/
+
+#define MAXN 2
+#define LL __int64
+int MOD = 1000000007;
+
+class Matrix {
+private:
+	int n, m;
+	LL** pkData;
+
+	static Matrix s_kMatrix[64], s_kMultiplyTemp;
+
+	void _Alloc() {
+		// 之所以把空间放在堆上，是因为当MAXN很大时，栈上分配不了这么多空间
+		pkData = new LL *[MAXN];
+		for (int i = 0; i < MAXN; ++i) {
+			pkData[i] = new LL[MAXN];
+		}
+	}
+
+	void _Dealloc() {
+		if (pkData) {
+			// 先释放低维空间，再释放高维的
+			for (int i = 0; i < MAXN; ++i) {
+				delete[] pkData[i];
+			}
+			delete[] pkData;
+			pkData = NULL;
+		}
+	}
+public:
+	Matrix() : n(0), m(0), pkData(NULL) {
+		_Alloc();
+	}
+	virtual ~Matrix() {
+		_Dealloc();
+	}
+
+	LL get(int i, int j) const {
+		return pkData[i][j];
+	}
+
+	int getN() const {
+		return n;
+	}
+
+	static void Identity(int _n, Matrix& ret) {
+		ret.n = ret.m = _n;
+		int i, j;
+		for (i = 0; i < _n; i++) {
+			for (j = 0; j < _n; j++) {
+				ret.pkData[i][j] = (i == j) ? 1 : 0;
+			}
+		}
+	}
+
+	static void GetPow(Matrix& raw, unsigned __int64 e, Matrix& ret) {
+		int index = 1;
+		Matrix::Identity(raw.getN(), ret);       // 0次幂
+		Matrix::s_kMatrix[1].Copy(raw);          // 1次幂
+		while (e) {
+			if (e & 1) {
+				Matrix::Multiply(ret, Matrix::s_kMatrix[index], Matrix::s_kMultiplyTemp);
+				ret.Copy(Matrix::s_kMultiplyTemp);
+			}
+			e >>= 1;
+			Matrix::Multiply(Matrix::s_kMatrix[index], Matrix::s_kMatrix[index], Matrix::s_kMatrix[index + 1]);
+			++index;
+		}
+	}
+
+	static void Multiply(const Matrix &me, const Matrix& other, Matrix& ret) {
+		// assert(m == other.n);
+		ret.Reset(me.n, other.m);
+		int i, j, k;
+		for (i = 0; i < me.n; i++) {
+			for (k = 0; k < me.m; k++) {
+				LL v = me.pkData[i][k];
+				for (j = 0; j < other.m; j++) {
+					ret.pkData[i][j] += v * other.pkData[k][j];
+					if (ret.pkData[i][j] >= MOD) {
+						ret.pkData[i][j] %= MOD;
+					}
+				}
+			}
+		}
+	}
+
+	static void Add(const Matrix &me, const Matrix& other, Matrix& ret) {
+		ret.Reset(me.n, other.m);
+		int i, j;
+		for (i = 0; i < me.n; i++) {
+			for (j = 0; j < me.m; j++) {
+				ret.pkData[i][j] = (me.pkData[i][j] + other.pkData[i][j]) % MOD;
+			}
+		}
+	}
+
+	void Reset(int nn, int mm, LL data[][MAXN]) {
+		n = nn;
+		m = mm;
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j) {
+				pkData[i][j] = data[i][j];
+			}
+		}
+	}
+
+	void Reset(int nn, int mm) {
+		n = nn;
+		m = mm;
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j) {
+				pkData[i][j] = 0;
+			}
+		}
+	}
+
+	void Copy(const Matrix& other) {
+		n = other.n;
+		m = other.m;
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < m; ++j) {
+				pkData[i][j] = other.pkData[i][j];
+			}
+		}
+	}
+
+
+
+	// | A  A |
+	// | O  I |
+	// I 需要手动调用 Identity(n);
+	// 扩展矩阵用于求A + A^2 + A^3 + ... + A^n
+	void getExtendMatrix(Matrix& ret, Matrix& I) {
+		ret.n = ret.m = n * 2;
+		ret.Reset(n * 2, n * 2);
+		ret.copyMatrix(*this, 0, 0);
+		ret.copyMatrix(*this, 0, n);
+		ret.copyMatrix(I, n, n);
+	}
+
+	// 获取 this 矩阵的 (r,c) - (n,m) 的子矩阵存到 ret
+	void getSubMatrix(Matrix& ret, int r, int c, int n, int m) {
+		ret.n = n;
+		ret.m = m;
+		for (int i = r; i < r + n; i++) {
+			for (int j = c; j < c + m; j++) {
+				ret.pkData[i - r][j - c] = pkData[i][j];
+			}
+		}
+	}
+
+	// 将矩阵A拷贝到当期矩阵的(r, c)位置
+	void copyMatrix(Matrix& A, int r, int c) {
+		for (int i = r; i < r + A.n; i++) {
+			for (int j = c; j < c + A.m; j++) {
+				pkData[i][j] = A.pkData[i - r][j - c];
+			}
+		}
+	}
+
+	void Print() {
+		int i, j;
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < m; j++) {
+				printf("%d ", pkData[i][j]);
+			}
+			puts("");
+		}
+	}
+};
+
+Matrix Matrix::s_kMatrix[64];
+Matrix Matrix::s_kMultiplyTemp;
+
+LL AArray[MAXN][MAXN] = {
+	{ 3, 1 },
+	{ 1, 0 }
+};
+
+LL BArray[MAXN][MAXN] = {
+	{ 1 },
+	{ 0 }
+};
+
+LL Exp(LL a, LL b, LL c) {
+	if (b == 0) {
+		return 1 % c;
+	}
+	LL tmp = Exp(a*a % c, (b >> 1), c);
+	if (b & 1) {
+		tmp = tmp * a % c;
+	}
+	return tmp;
+}
+
+
+// 1. 构造系数矩阵
+// 2. 构造列向量
+// 3. 二分幂矩阵 * 列向量
+
+Matrix A, B, ret1, ret2;
+Matrix N;
+
+LL Enum(LL Mod) {
+	LL gpre, gnow, i;
+	gpre = 0;
+	gnow = 1;
+	for (i = 2;; ++i) {
+		LL tgnow = (gnow * 3 + gpre);
+		if (tgnow >= Mod) tgnow %= Mod;
+		gpre = gnow;
+		gnow = tgnow;
+		if (gnow == 1 && gpre == 0) {
+			printf("%I64d\n", i);
+			return i;
+		}
+	}
+}
+
+int main() {
+	int i;
+	LL n;
+	A.Reset(2, 2, AArray);
+	B.Reset(2, 1, BArray);
+
+	while (scanf("%I64d", &n) != EOF) {
+		if (n <= 1) {
+			printf("%d\n", (int)n);
+		}
+		else {
+			LL ans = 0;
+
+
+			LL N = n;
+			MOD = 183120;
+			// g(n) % 183120
+			{
+				Matrix::GetPow(A, N - 1, ret1);
+				Matrix::Multiply(ret1, B, ret2);
+				ans = ret2.get(0, 0);
+				if (ans == 0) {
+					ans = MOD;
+				}
+			}
+
+			N = ans;
+			MOD = 222222224;
+			// g( g(n) ) % 222222224
+			{
+
+				Matrix::GetPow(A, N - 1, ret1);
+				Matrix::Multiply(ret1, B, ret2);
+				ans = ret2.get(0, 0);
+				if (ans == 0) {
+					ans = MOD;
+				}
+			}
+
+			N = ans;
+			MOD = 1000000007;
+			// g( g( g(n) ) ) % 1000000007
+			{
+
+				Matrix::GetPow(A, N - 1, ret1);
+				Matrix::Multiply(ret1, B, ret2);
+				ans = ret2.get(0, 0);
+			}
+
+			printf("%I64d\n", ans);
+		}
+	}
+
+	return 0;
+}
