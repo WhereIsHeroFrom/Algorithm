2416b6738352940ad21f4351675ec4f22c79a5d3
WhereIsHeroFrom <menjitianya2007@163.com>
2021-06-23 09:00:35 +0800

    提交中国剩余定理相关题目

--- 数学/数论/中国剩余定理/HDU 5668 Circle.cpp
+++ 数学/数论/中国剩余定理/HDU 5668 Circle.cpp
@@ -1,145 +1,178 @@
 #include <iostream>
-#include <cstring> 
+#include <cstring>
+
 using namespace std;
 
-#define LL __int64
-#define MAXN 100010
+#define ll long long
+const int MAXN = 100010;
 
-LL GCD(LL a, LL b) {
-	return b ? GCD(b, a%b) : a;
+
+ll GCD(ll a, ll b) {
+    return b ? GCD(b, a%b) : a;
 }
+
+class ModPair {
+    /*
+        x = km + a
+    */
+private:
+    ll m_;   // modulus
+    ll a_;   // remainder
+public:
+    ModPair() {}
+    ModPair(ll m, ll a) : m_(m), a_(a) {}
+
+    // 标准化，将 m 和 a 都转化为正数
+    void standardization() {
+        if(m_ < 0) {
+            m_ = -m_;
+        }
+        a_ = (a_ % m_ + m_) % m_;
+    }
+
+    ll lcm (ll om) const {
+        ll g = GCD(om, m_);
+        return om / g * m_;
+    }
+
+    ll getModulus() const { return m_;}
+    void setModulus(ll m) {m_ = m;}
+    ll getRemainder() const { return a_;}
+    void setRemainder(ll a) {a_ = a;} 
+};
+
 
 // 扩展欧几里得
 // aX + bY = 1 
-LL ExpGcd(LL a, LL b, LL &X, LL &Y) {
-	LL q, temp;
-	if (!b) {
-		q = a; X = 1; Y = 0;
-		return q;
-	}
-	else {
-		q = ExpGcd(b, a % b, X, Y);
-		temp = X;
-		X = Y;
-		Y = temp - (a / b) * Y;
-		return q;
-	}
+ll ExpGcd(ll a, ll b, ll &X, ll &Y) {
+    ll q, temp;
+    if( !b ) {
+        q = a; X = 1; Y = 0;
+        return q;
+    }else {
+        q = ExpGcd(b, a % b, X, Y);
+        temp = X; 
+        X = Y;
+        Y = temp - (a / b) * Y;
+        return q;
+    }
 }
 
-// K = m[0] * x + r[0];
-// K = m[1] * y + r[1];
-// K = m[2] * z + r[2];
-
-// 计算出y的表达式 m[0] * x - m[1] * y = r[1] - r[0];
-// y = m[3] * v + r[3];
-
+// long long 乘法，非溢出版本 
+ll Product_Mod(ll a, ll b, ll mod) {
+   ll sum = 0;
+   int flag = (b >= 0 ? 1 : -1);
+   if(flag < 0) {
+       b = -b;
+   }
+   while(b) {
+       if(b & 1) 
+           sum = (sum + a) % mod;
+       a = (a + a) % mod;
+       b >>= 1;
+   }
+   return (sum * flag + mod) % mod;
+}
 
 // 中国剩余定理 模板
-// -1 表示无解 
-LL chineseRemain(int n, LL mod[], LL rem[]) {
-	LL lcm = 1;
-	int i;
-	// 1. 预处理，将所有的mod[]和rem[]都转化成正整数
-	for (i = 0; i < n; ++i) {
-		// K = x[i]*mod[i] + rem[i];     (K为最终解，x[i]为未知数)
-		// a.除数取正 
-		if (mod[i] < 0) {
-			mod[i] = -mod[i];
-		}
-		// b.余数取正
-		rem[i] = (rem[i] % mod[i] + mod[i]) % mod[i];
-	}
-	// 2.计算所有被模数的最小公倍数 
-	for (i = 0; i < n; i++) {
-		LL g = GCD(lcm, mod[i]);
-		lcm = lcm / g * mod[i];
-	}
-	// 3.算法主流程 （令最终解为K） 
-	//   枚举 i = 1 to n-1 
-	//   1) 合并等式(0)和(i)， mod[0]*x[0] + rem[0]      = mod[i]*x[i] + rem[i];
-	//                         mod[0]*x[0] - mod[i]*x[i] = rem[i] - rem[0];
-	//                            A  *  X  +   B   * Y   = C;
-	//   2) 根据扩展欧几里得，求得x[i] = Y = Y0 + A*t; 
-	//   3) 将x[i]代入等式(i)，得到：   K = mod[0]*mod[i]*t + (rem[i]+mod[i]*Y0); 
-	//   4) 更新等式(0)，    mod[0] = mod[0]*mod[i]
-	//                       rem[0] = (rem[i]+mod[i]*Y0) % mod[0];
-	//   5) n-1次迭代完毕，rem[0]就是最小非负整数解。 
-	LL A, B, C, X, Y;
-	for (i = 1; i < n; i++) {
-		A = mod[0];
-		B = -mod[i];
-		C = rem[i] - rem[0];
-		LL g = GCD(A, B);
-		if (C % g) {
-			return -1;
-		}
-		// 等式两边同时除上g或-1，等式不变 
-		A /= g, B /= g, C /= g;
-		if (A < 0) {
-			// 这一步是为了把A和B都转化成正数 
-			A = -A, B = -B, C = -C;
-			B = ((B % A) + A) % A;
-		}
-		ExpGcd(A, B, X, Y);
-		Y = ((Y % A * C) % A + A) % A;
-		mod[0] = A*mod[i];
-		rem[0] = (rem[i] + mod[i] * Y) % mod[0];
-	}
-	if (rem[0] == 0) {
-		rem[0] += lcm;
-	}
-	return rem[0];
+// return -1 表示无解 
+ModPair chineseRemain(int n, ModPair* mp) {
+    /********************(1)********************/
+    for(int i = 0; i < n; ++i) {
+        mp[i].standardization();
+    } 
+    ModPair ans = mp[0];
+    ll A, B, C, X, Y;
+    for(int i = 1; i < n; i++) {
+        /********************(2)********************/
+        A = ans.getModulus();         
+        B = -mp[i].getModulus();
+        C = mp[i].getRemainder() - ans.getRemainder();
+        ll g = GCD(A, B);
+        if( C % g ) {     
+            ans.setRemainder(0);
+            ans.setModulus(-1);             
+            return ans;
+        }
+        /********************(3)********************/
+        A /= g, B /= g, C /= g;
+        /********************(4)********************/
+        if(A < 0) {
+            A = -A, B = -B, C = -C;
+            B = ((B % A) + A) % A;
+        }
+        /********************(5)********************/
+        ExpGcd(A, B, X, Y);
+        Y = Product_Mod(Y, C, A);
+        /********************(6)********************/
+        ll tmpm = A * mp[i].getModulus();
+        ll tmpr = (mp[i].getRemainder() + Product_Mod(mp[i].getModulus(), Y, tmpm)) % tmpm;
+        
+        ans.setModulus(tmpm);
+        ans.setRemainder(tmpr);
+    }
+    return ans;
 }
 
-LL m[MAXN], r[MAXN];
+ 
+
+ModPair mp[MAXN];
+ll m[MAXN], r[MAXN];
 int top;
 int pos[MAXN], hashv[MAXN];
 int n;
 
 int main() {
-	int t;
-	int i, x;
+    int t;
+    int i, x;
+    
+    scanf("%d", &t);
+    while(t--) {
+        scanf("%d", &n);
+        for(i = 1; i <= n; i++) {
+            scanf("%d", &x);
+            pos[x] = i;
+        }
+        memset(hashv, 0, sizeof(hashv));
+        top = 1;
+        int x = 0;
+        int cnt = 0;
+        while(top <= n) {
+            cnt = 0;
+            while(1) {
+                // 模拟约瑟夫环 
+                if(++x > n) {
+                    x = 1;
+                }
+                // 如果这个位置还有人在 
+                if(!hashv[x]) {
+                    ++cnt;
+                    if(x == pos[top]) {
+                        hashv[x] = 1;
+                        m[top-1] = n-top+1, r[top-1] = cnt;
+                        
+                        //printf("%I64d %I64d\n", m[top-1], r[top-1]);
+                        break;
+                    }    
+                }
+            }
+            ++top;
+        }
 
-	scanf("%d", &t);
-	while (t--) {
-		scanf("%d", &n);
-		for (i = 1; i <= n; i++) {
-			scanf("%d", &x);
-			pos[x] = i;
-		}
-		memset(hashv, 0, sizeof(hashv));
-		top = 1;
-		int x = 0;
-		int cnt = 0;
-		while (top <= n) {
-			cnt = 0;
-			while (1) {
-				// 模拟约瑟夫环 
-				if (++x > n) {
-					x = 1;
-				}
-				// 如果这个位置还有人在 
-				if (!hashv[x]) {
-					++cnt;
-					if (x == pos[top]) {
-						hashv[x] = 1;
-						m[top - 1] = n - top + 1, r[top - 1] = cnt;
-						//printf("%I64d %I64d\n", m[top-1], r[top-1]);
-						break;
-					}
-				}
-			}
-			++top;
-		}
-		LL ans = chineseRemain(n, m, r);
-		if (ans == -1) {
-			printf("Creation August is a SB!\n");
-		}
-		else {
-			printf("%I64d\n", ans);
-		}
-	}
-	return 0;
+        for(int i = 0; i < n; ++i) {
+            mp[i] = ModPair(m[i], r[i]);
+        }
+        ModPair ans = chineseRemain(n, mp);
+        if(ans.getModulus() == -1) {
+            printf("Creation August is a SB!\n");
+        }else {
+            if(ans.getRemainder() == 0) {
+                ans.setRemainder( ans.getModulus() );
+            }
+            printf("%lld\n", ans.getRemainder());
+        }
+    }
+    return 0;
 }
 
 /*
